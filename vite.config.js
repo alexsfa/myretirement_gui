3140bf1811e6e3f1ab6d43e7f1d25c1a870022b5
sfa <alexandrossfak000@gmail.com>
2025-08-27 16:29:28 +0300

    define VITE host dynamically

--- vite.config.js
+++ vite.config.js
@@ -5,26 +5,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    hmr: {
-      host: env.dev.VITE_DEV_HOST || 'localhost', 
-      protocol: 'ws',
+    server: {
+      host: '0.0.0.0',
+      hmr: {
+        host: env.VITE_DEV_HOST || 'localhost', // ✅ now works
+        protocol: 'ws',
+      },
     },
-  },
+  }
 })
 
 
